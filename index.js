38ce76203bdc0b6aa1430c0d46152118eb4d4407
mmm0707 <amirn.0927mcr@gmail.com>
2022-05-29 22:08:50 +0900

    修正　JS

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 //値取得
 const login = document.getElementById('login');
-const userTelValue = document.getElementById('tel').value;
-const userPassValue = document.getElementById('password').value;
-
+const signup = document.getElementById('signup');
 
 //仮ユーザー情報を設定
 let userTel = '123';
 let userPass = '456';
 
 //新規ユーザー登録処理
-// newuser.addEventListener('click', e) =>{
-//     let newUserTel = document.getElementById('newTel').value;
-//     let newUserPassword = document.getElementById('newPassword').value;
+// newuser.addEventListener('click', e => {    
+// const newTel = document.getElementById('newTel').value;
+// const newPass = document.getElementById('newPass').value;
 // });
 
 //ログイン処理
 //te: 11桁入力、ハイフンなし　pw:8～10桁　半角英数字　大文字小文字含む
 login.addEventListener('click', e => {
+    const userTelValue = document.getElementById('tel').value;
+    const userPassValue = document.getElementById('password').value;
     if (3 <= userTelValue.length || 3 <= userPassValue.length) {
         if (userTel === userTelValue && userPass === userPassValue) {
             console.log('ok!');
