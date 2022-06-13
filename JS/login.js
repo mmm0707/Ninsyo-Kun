7cdd246d178c51a21e913d11308236b5ee11242a
humpzin <humpzin@gmail.com>
2022-06-14 05:55:09 +0900

    fix

--- JS/login.js
+++ JS/login.js
@@ -1,23 +1,44 @@
-//値取得
-const login = document.getElementById('login');
+// XMLHttpRequestオブジェクトの作成
+var request = new XMLHttpRequest();
+var endpoint = "https://oafgomqqvg.execute-api.us-east-1.amazonaws.com/APItest/dynamodbctrl";
 
-//仮ユーザー情報を設定
-let userTel = '123';
-let userPass = '456';
+// URLを開く
+request.open('POST', endpoint, true);
 
-//ログイン処理
-//tel: 11桁入力、ハイフンなし　pw:8～10桁　半角英数字　大文字小文字含む
-login.addEventListener('click', e => {
-    const userTelValue = document.getElementById('tel').value;
-    const userPassValue = document.getElementById('password').value;
-    if (3 <= userTelValue.length || 3 <= userPassValue.length) {
-        if (userTel === userTelValue && userPass === userPassValue) {
-            console.log('ok!');
-            console.log(userTelValue.length);
-        } else {
-            console.log('電話番号もしくはパスワードが間違っています。');
-        }
-    } else {
-        console.log('文字数が不足しています。');
+// リクエストをURLに送信
+request.send();
+
+// レスポンスが返ってきた時の処理を記述 
+request.onload = function (e) {
+    // レスポンスが返ってきた時の処理
+    if (this.status === 200) {
+        console.log(request.responsText);
     }
-});
+};
+
+function login() {
+    //値取得
+    const login = document.getElementById('login');
+
+    //仮ユーザー情報を設定
+    let userTel = '123';
+    let userPass = '456';
+
+    //ログイン処理
+    //tel: 11桁入力、ハイフンなし　pw:8～10桁　半角英数字　大文字小文字含む
+    login.addEventListener('click', e => {
+        const userTelValue = document.getElementById('tel').value;
+        const userPassValue = document.getElementById('password').value;
+        if (3 <= userTelValue.length || 3 <= userPassValue.length) {
+            if (userTel === userTelValue && userPass === userPassValue) {
+                console.log('ok!');
+                console.log(userTelValue.length);
+            } else {
+                console.log('電話番号もしくはパスワードが間違っています。');
+            }
+        } else {
+            console.log('文字数が不足しています。');
+        }
+    });
+}
+
