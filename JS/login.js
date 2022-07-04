0d73326b903d91ef5420fcffe1445a635dae8fa5
mmm0707 <amirn.0927mcr@gmail.com>
2022-07-05 06:46:03 +0900

    index.html　削除

--- JS/login.js
+++ JS/login.js
@@ -4,20 +4,21 @@ login.addEventListener('click', function () {
 
     //APIGateWayの認証機能エンドポイントにリクエストする…vscode
     //DynamoDB中にIDとPASSの組み合わせに該当するものがあれば認証する…aws内に関数を作る
+
+    //すること： IDとPWを取得　フォームの！
+    const tel = document.getElementById('tel');
+    const password = document.getElementById('password');
+
+    //すること：取得したIDとPWを送る　＝　POSTする！GETじゃない！
     var req = new XMLHttpRequest();
     req.open(
-        "GET",
+        "POST",
         "https://oafgomqqvg.execute-api.us-east-1.amazonaws.com/APItest/dynamodbctrl"
     );
-    req.send();
-    req.onreadystatechange = function () { //onreadystatechangeはreadyState 属性が変化するたびに呼び出す
-        var data = JSON.parse(req.responseText || "null"); //JSONの読み込み
-        //認証OKであれば『トップページ』へ。
-        if (data) {
-            console.log(data);
-            // } else {
-            //     //認証NGであればアラートを出す。
-            //     alert('電話番号もしくはパスワードが違います。');
-        }
-    };
+    req.send(tel, password);
+
+    //IDとPWとがあっていたかAWS側からのレスポンスを受け取る、見る
+    //それがOKなのかERRなのかを受け取る
+    //OKであればTOPページへ
+    //ERRであればアラート表示
 });
