2017b9a8116b79c3c4b716bc29342a7e60aa6bab
mmm0707 <amirn.0927mcr@gmail.com>
2022-07-05 05:21:17 +0900

    login.js 変更　認証機能

--- JS/login.js
+++ JS/login.js
@@ -1,8 +1,23 @@
 //認証ボタンをトリガーとする
+const login = document.getElementById('login-button');
+login.addEventListener('click', function () {
 
-//APIGateWayの認証機能エンドポイントにリクエストする
-//DynamoDB中にIDとPASSの組み合わせに該当するものがあれば認証する
-
-//認証OKであれば②『トップページ』へ。
-
-//認証NGであればアラートを出す。
+    //APIGateWayの認証機能エンドポイントにリクエストする…vscode
+    //DynamoDB中にIDとPASSの組み合わせに該当するものがあれば認証する…aws内に関数を作る
+    var req = new XMLHttpRequest();
+    req.open(
+        "GET",
+        "https://oafgomqqvg.execute-api.us-east-1.amazonaws.com/APItest/dynamodbctrl"
+    );
+    req.send();
+    req.onreadystatechange = function () { //onreadystatechangeはreadyState 属性が変化するたびに呼び出す
+        var data = JSON.parse(req.responseText || "null"); //JSONの読み込み
+        //認証OKであれば『トップページ』へ。
+        if (data) {
+            console.log(data);
+            // } else {
+            //     //認証NGであればアラートを出す。
+            //     alert('電話番号もしくはパスワードが違います。');
+        }
+    };
+});
